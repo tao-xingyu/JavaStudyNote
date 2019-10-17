36dc80b185a5ff7ef8de3ab32897a450048db85f
tao-xingyu <1396563360@qq.com>
2019-10-17 09:43:17 +0800

    [ADD]:Stream API 映射与排序

--- JavaSE-高级/14_Java8新特性/src/code/d/stream/StreamApi1Test.java
+++ JavaSE-高级/14_Java8新特性/src/code/d/stream/StreamApi1Test.java
@@ -3,97 +3,79 @@ package code.d.stream;
 import code.c.methodreferences.Employee;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 /**
- * Stream中间操作测试
+ * Stream Api 实例化测试
+ * <p>
+ * 1.Stream关注的是对数据的运算，与cpu打交道
+ * 集合关注的是数据的存储，与内存打交道
+ * <p>
+ * 2.  ① Stream自己不会存储元素
+ * ② Stream 不会改变源对象。相反，他们会返回一个持有结果的新Stream
+ * ③ Stream 操作是延迟执行的。这意味着他们会等到需要结果的时候才执行
+ * <p>
+ * 3. Stream 执行流程
+ * ① Stream的实例化
+ * ② 一系列的中间操作（过滤、映射、...）
+ * ③ 终止操作
+ * 4. 说明：
+ * ①一个中间操作链，对于源数据进行处理
+ * ②一旦执行终止操作，就执行中间操作链，并产生结果。之后，不会再被使用
  *
  * @author Jerry
- * @create 2019-10-15 22:44
+ * @create 2019-10-15 14:57
  */
 public class StreamApi1Test {
     /**
-     * 一、筛选与切片
-     * <p>
-     * filter(Predicate p)接收Lambda,从流中排除某些元素
-     * <p>
-     * distinct() 筛选，通过流所生成元素的hashCode和equals()去除重复元素
-     * <p>
-     * limit(long maxSize) 截断流，使元素不超过给定数量
-     * <p>
-     * skip(long n) 跳过元素，返回一个扔掉了前n个元素的流、若元素中元素不足n个，则返回一个空流，与limit(n)互补
+     * 创建Stream 方式一 通过集合
      */
     @Test
     public void test01() {
         List<Employee> employees = EmployeeData.getEmployees();
-        System.out.println("以下是排除流:");
+        //顺序流
         Stream<Employee> stream = employees.stream();
-        //排除 filter(Predicate p)
-        stream.filter(e -> e.getSalary() > 7000).forEach(System.out::println);
-        System.out.println("以下是截断流:");
-        Stream<Employee> stream1 = employees.stream();
-        //截断  limit(n)
-        stream1.limit(5).forEach(System.out::println);
-        System.out.println("以下是筛选流:");
-        //筛选 distinct() 通过流所生成元素的hashCode和equals()去除重复元素
-        employees.add(new Employee(10010, "拿破仑", 200, 3000000));
-        employees.add(new Employee(10010, "拿破仑", 201, 3000000));
-        employees.add(new Employee(10010, "拿破仑", 200, 3000000));
-        employees.add(new Employee(10010, "拿破仑", 200, 3000000));
-        employees.add(new Employee(10010, "拿破仑", 200, 3000000));
-        Stream<Employee> stream2 = employees.stream();
-        stream2.distinct().forEach(System.out::println);
-        System.out.println("以下是跳过流:");
-        //跳过 skip(n)
-        Stream<Employee> stream3 = employees.stream();
-        stream3.skip(3).forEach(System.out::println);
+        //并行流
+        Stream<Employee> parallelStream = employees.parallelStream();
     }
 
     /**
-     * 二、映射
+     * 方式二 通过数组
      */
     @Test
     public void test02() {
-        //map(Function f)接收一个函数作为参数，该函数会被应用到每个元素上，并将器映射成一个新元素
-        List<String> list = Arrays.asList("aa", "bb", "cc", "dd");
-        System.out.println("map中间操作将流中的小写变成大写：");
-        list.stream().map(str -> str.toUpperCase()).forEach(System.out::println);
-        System.out.println("map中间操作获取员工姓名长度大于3的员工姓名：");
-        //获取员工姓名长度大于3的员工姓名
-        //1.获取员工姓名流
-        List<Employee> employees = EmployeeData.getEmployees();
-        Stream<String> namesStream = employees.stream().map(Employee::getName);
-        //2.从姓名流中排除姓名长度小于3的员工姓名
-        namesStream.filter(str -> str.length() > 3).forEach(System.out::println);
-        System.out.println("flatMap中间操作：");
-        //flatMap(Function f) 接收一个函数作为参数，将流中的每个值都换成另一个流，然后把所有的流连接成一个流
-        list.stream().flatMap(StreamApi1Test::fromStringToStream).forEach(System.out::println);
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6};
+        IntStream stream = Arrays.stream(arr);
 
+        Employee e1 = new Employee(10086, "Tom");
+        Employee e2 = new Employee(10087, "Jerry");
+        Employee[] arr1 = new Employee[]{e1, e2};
+        Stream<Employee> stream1 = Arrays.stream(arr1);
     }
 
     /**
-     * 将字符串中多个字符构成的集合转换成Stream实例
-     *
-     * @param str 字符串
-     * @return 转换后的字符流
-     */
-    public static Stream<Character> fromStringToStream(String str) {
-        List<Character> list = new ArrayList<>();
-        for (Character character : str.toCharArray()) {
-            list.add(character);
-        }
-        return list.stream();
-    }
-
-    /**
-     * 三、排序
+     * 方式三 通过Stream 的of
      */
     @Test
     public void test03() {
-        //sorted() 产生一个新流，其中按自然顺序排序
-        //sorted(Comparator com) 产生一个新流，其中按比较器顺序排序
+
+        Stream<Integer> integerStream = Stream.of(1, 2, 3, 4, 5, 6);
+    }
+
+    /**
+     * 方式四 创建无限流
+     */
+    @Test
+    public void test04(){
+        //迭代
+        //public static<T> Stream<T> iterate(final T seed,final UnaryOperator<T> f)
+        //遍历前十个偶数
+        Stream.iterate(0,t->t+2).limit(10).forEach(System.out::println);
+        //生成
+        //public static<T> Stream<T> generate(Supplier<T> s)
+        Stream.generate(Math::random).limit(10).forEach(System.out::println);
     }
 }
---
+++ JavaSE-高级/14_Java8新特性/src/code/d/stream/StreamApi3Test.java
@@ -0,0 +1,8 @@
+package code.d.stream;
+
+/**
+ * @author Jerry
+ * @create 2019-10-17 9:38
+ */
+public class StreamApi3Test {
+}
